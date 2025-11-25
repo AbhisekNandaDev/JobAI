453f90caec892446676c15d748aa76474ab61627
Abhisek Nanda <abhiseknanda.dev@gmail.com>
2025-11-26 01:35:24 +0530

    Update jwt_auth.py

--- jwt_auth.py
+++ jwt_auth.py
@@ -9,7 +9,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_jwt_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    expire = datetime.now(timezone.utc) 
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY.encode(), algorithm=JWT_ALGORITHM)
 
